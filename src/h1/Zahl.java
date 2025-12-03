97b288d684d618eabe3d31417792c3c7310465d9
Nawid Adel <nawid@MacBook-Air-von-Nawid.local>
2025-12-03 15:12:44 +0100

    h1

---
+++ src/h1/Zahl.java
@@ -0,0 +1,20 @@
+package h1;
+
+public class Zahl {
+	public boolean even = false;
+	public boolean small = false;
+	public boolean positive = false;
+	public int num = 0;
+
+	public void setEven() {
+		even = num % 2 == 0;
+	}
+
+	public void setSmall() {
+		small = num < 100;
+	}
+
+	public void setPositive() {
+		positive = num > 0;
+	}
+}
